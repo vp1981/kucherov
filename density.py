797f723f526653ca24be292f58862754d0a9489a
user <kucherov145@gmail.com>
2024-11-29 20:08:21 +0800

    Правки в программе

--- density.py
+++ density.py
@@ -1,10 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-r = lambda x, o: np.sqrt(4*np.cos(o)**2*(x**2-x)+1)
-def density(x):
+def distance(x, o):
+    return np.sqrt(4*np.cos(o)**2*(x**2-x)+1)
+
+def sun(x):
     """
-    Calculate density at a distance of x
+    Calculate density of the sun at a distance of x
     """
 
     return 6.5956*1e4*np.exp(-10.54*x)
 
+def supernova(x):
+    """
+    Calculate density of a supernova at a distance of x
+    """
+
+    return 52.934/x**3
+def earth(x):
+    pass
+
+density_sun = lambda x, o: sun(distance(x, o))
+density_SN = lambda x, o: supernova(distance(x, o))
+density_earth = lambda x, o: earth(distance(x, o))
