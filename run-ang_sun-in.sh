5049556dde4f0660acf9a9e3f906791f8ebaf5a5
danilakucher <kucherov145@gmail.com>
2025-05-02 09:57:02 +0800

    доработка сценария run-ang_sun_in.sh
      - сценарий доработан не до конца.

--- run-ang_sun-in.sh
+++ run-ang_sun-in.sh
@@ -4,12 +4,32 @@ export LC_NUMERIC=en_US.UTF-8
 
 fk=(0.9 0.95 1.0 1.05 1.1)
 model="sun"
-fdata="data-${model}.tmp"
+fdata="/tmp/data-${model}.tmp"
+datadir="$PWD/data"
+bindir="$PWD/magexp/bin"
+prog=m4-tol
 mod="in"
 mang="s12"
 Ep1=-3
 Ep2=7
 N=50
+
+if [ ! -d $datadir ];then
+  mkdir $datadir
+fi
+
+if [ ! -d $bindir ];then
+  echo "ошибка: нет каталога '$bindir'"
+  exit 1
+fi
+
+(( $# !=3 )) && 
+{
+  echo "сценарий требует 3 аргумента: маркер угла, количество шагов по энергии, количество шагов фактора угла"
+  echo "пример: "
+  echo "  $0 s12|s13 50 5"
+  exit 2
+}
 
 case $1 in
   "s12")
@@ -19,9 +39,27 @@ case $1 in
     mang="s13"
     ;;
   *)
-    echo "using default angle ${mang}"
+    echo "ошибка: нераспознанный маркер: $1"
+    exit 3
     ;;
 esac
+
+Ne="$2"
+Nf="$3"
+
+(( Nf/2 != (Nf-1)/2 )) &&
+{
+  echo "ошибка: количество шагов для фактора угла должно быть нечетным, Nf=${Nf}"
+  exit 4
+}
+
+if [ ! -e $bindir/$prog ]; then
+  echo "ошибка: нет расчетной программы $prog"
+  exit 5
+fi
+
+tdir=${datadir}/${model}_${mod}_${mang}/"NexNf=${Ne}x${Nf}"
+mkdir -p ${tdir}
 
 for ex in "${fk[@]}"
 do
