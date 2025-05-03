88da46dbd8397892ed46c69f53d88640c5d3e594
Vladimir Lomov <vladimir@theor.ktp>
2025-05-03 10:13:26 +0800

    Fixed call string
    Signed-off-by: Vladimir Lomov <vladimir@theor.ktp>

--- run-ang_sun-in.sh
+++ run-ang_sun-in.sh
@@ -91,7 +91,7 @@ do
 	echo -n "" > "${datf}"
   for i in $(seq 0 1 $((Ne-1)))
 	do
-    ./${prog} "${model_file}" -c "Ep1=${Ep1};Ep2=${Ep2};d=(Ep2-Ep1)/(${N}-1);E=math.exp((${Ep1}+${i}*d)*math.log(10));${mang}=${ex}*${mang}" "${prob_file}" > "${fdata}"
+    ./${prog} "${model_file}" -c "Ep1=${Ep1};Ep2=${Ep2};d=(Ep2-Ep1)/(${Ne}-1);E=math.exp((Ep1+${i}*d)*math.log(10));${mang}=${ex}*${mang}" "${prob_file}" > "${fdata}"
 		dat=($(grep -v '^#' "${fdata}"))
 		ang=$(grep "#*ex.*${mang}" "${fdata}" | sed -e 's@.*=@@')
 		echo "${dat[0]}  ${dat[1]}  ${dat[2]} ${ang} ${ex}" >> "${datf}"
